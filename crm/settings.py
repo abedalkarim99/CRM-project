5e66117eb2bc96312f93783b863eb2cda3a36eb9
abedalkarim99 <abedalkarim215@gmail.com>
2020-07-12 01:56:29 +0300

    add all files

--- crm/settings.py
+++ crm/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qik@!po7x1nyq76*w_(7nu$h)j7@bf-dkgw0(ir&y8ed$j+^=d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
